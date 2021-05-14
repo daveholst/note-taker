8c42337aeebe0562abb65cf2a173960f8b4d1b1d
Dave Holst <daveholst@gmail.com>
2021-05-14 17:20:14 +0800

    notes saving to database

--- server.js
+++ server.js
@@ -12,19 +12,33 @@ const { PORT } = process.env;
 
 // create a static route to the public folder
 app.use(express.static('public'));
+// had to add this to get the body parsing from browser on POST
+app.use(express.json());
 
 // create route for /notes to notes.hmtl
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-// API //
+// answering the GET request for the notes!//
 app.get('/api/notes', async (req, res) => {
   const dbData = await fs.readFile(
     path.join(__dirname, '/db/db.json'),
     'utf-8'
   );
   res.json(JSON.parse(dbData));
+});
+// answering the POST request for the notes!
+app.post('/api/notes', async (req, res) => {
+  // db to object
+  let dbData = await fs.readFile(path.join(__dirname, '/db/db.json'), 'utf-8');
+  dbData = JSON.parse(dbData);
+  dbData.push(req.body);
+  await fs.writeFile(
+    path.join(__dirname, 'db/db.json'),
+    JSON.stringify(dbData, null, 2)
+  );
+  res.end();
 });
 
 // every other request should go to to the 'home page'
