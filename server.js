1f91530be066e5403c6fe0bcf8c1dae26db2c8e8
Dave Holst <daveholst@gmail.com>
2021-05-14 16:47:40 +0800

    get requests for notes working

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
+const fs = require('fs').promises;
 
 const app = express();
 const { PORT } = process.env;
@@ -15,6 +16,20 @@ app.use(express.static('public'));
 // create route for /notes to notes.hmtl
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// API //
+app.get('/api/notes', async (req, res) => {
+  const dbData = await fs.readFile(
+    path.join(__dirname, '/db/db.json'),
+    'utf-8'
+  );
+  res.json(JSON.parse(dbData));
+});
+
+// every other request should go to to the 'home page'
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(PORT, () => console.log('Server up on Port:', PORT));
