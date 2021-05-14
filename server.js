b46b0240dc6af270c3b3e1c5292e984b223ff512
Dave Holst <daveholst@gmail.com>
2021-05-14 13:11:18 +0800

    /notes -> notes.html routing working

--- server.js
+++ server.js
@@ -2,19 +2,18 @@
 // server notes up @ /notes
 // build api for database
 
-
 const express = require('express');
 const path = require('path');
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 // create a static route to the public folder
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // create route for /notes
 app.get('/notes', (req, res) => {
-  res.sendFile('./public/notes.html')
-})
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
-app.listen(PORT, () => console.log('Server up on Port:',PORT))
+app.listen(PORT, () => console.log('Server up on Port:', PORT));
