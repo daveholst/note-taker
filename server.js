229e5683c4ecef084314ce627df58ab70112fa10
Dave Holst <daveholst@gmail.com>
2021-05-14 13:28:20 +0800

    updated config + .env for heroku

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 // server index html @ /
 // server notes up @ /notes
 // build api for database
+require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const { PORT } = process.env;
 
 // create a static route to the public folder
 app.use(express.static('public'));
 
-// create route for /notes
+// create route for /notes to notes.hmtl
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
